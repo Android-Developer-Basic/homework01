b52b3924ba2d0332e4841507f5183d15e2c0f423
Grave1ine <vladislav_barinov@bk.ru>
2023-04-15 13:31:20 +0300

    ,,,

--- src/main/kotlin/ru/otus/homework/fizzbuzz.kt
+++ src/main/kotlin/ru/otus/homework/fizzbuzz.kt
@@ -1,5 +1,35 @@
 package ru.otus.homework
 
+fun main() {
 
-fun fizzbuzz(n: Int): Array<String> {
+    val n: Int = 40
+
+    fizzbuzz(n)
+
 }
+fun fizzbuzz(n: Int): Array<String> {
+
+    val array = Array<String>(n){""}
+
+    for(i in 0 until n){          //until до n не включая
+
+        if(i == 0){
+            array[i] = "FizzBuzz"
+            } else if (i % 3 == 0){
+            array[i] = "Fizz"
+            } else if (i % 5 == 0){
+            array[i] = "Buzz"
+            } else if ((i % 3 == 0) && (i % 5 == 0)){
+            array[i] = "FizzBuzz"
+            } else {
+            array[i] = i.toString()
+        }
+
+        println(array[i])
+    }
+
+
+    return array
+}
+
+
--- src/main/kotlin/ru/otus/homework/sumoftwo.kt
+++ src/main/kotlin/ru/otus/homework/sumoftwo.kt
@@ -1,5 +1,35 @@
 package ru.otus.homework
 
 
-fun sumOfTwo(numbers: IntArray, target: Int): IntArray {
+
+fun main(){
+
+    val target = 9
+    val numbers: IntArray = intArrayOf(1, 5, 7, 8 , 3, 4)
+
+    val mass = sumOfTwo(numbers, target)
+
+    for(i in mass.indices) println(mass[i])
+
 }
+
+fun sumOfTwo(numbers: IntArray, target: Int): IntArray {
+
+    val result = IntArray(2)
+
+    for (i in numbers.indices){
+        for (j in numbers.indices){
+            if (target == (numbers[i] + numbers[j])){
+                result[0] = i
+                result[1] = j
+            }
+        }
+    }
+
+        if (result.all { it == 0 }) {
+            throw IllegalArgumentException()
+        }
+
+return result
+}
+
