6226ba39ae12af26b8d5f6febfd05e13faff8e3c
Ed_Krupenin <ed.krupenin@gmail.com>
2023-04-16 15:48:24 +0300

    Task 2
    Validation of input data, Creating an array for the answer, changing the array according to the conditions of the task

--- src/main/kotlin/ru/otus/homework/sumoftwo.kt
+++ src/main/kotlin/ru/otus/homework/sumoftwo.kt
@@ -2,4 +2,21 @@ package ru.otus.homework
 
 
 fun sumOfTwo(numbers: IntArray, target: Int): IntArray {
+    if (numbers.size < 2)
+        throw IllegalArgumentException()
+    val output = ArrayList<Int>()
+    for (externalLoop in numbers.indices) { // from i to size, where i = 0...n
+        var internalLoop = externalLoop + 1
+        while (internalLoop < numbers.size) {   // from j to size. where j = (i+1)...n
+            if (numbers[externalLoop] + numbers[internalLoop] == target) {
+                output.add(externalLoop)
+                output.add(internalLoop)
+            }
+            ++internalLoop
+        }
+    }
+    if (output.isEmpty()) {
+        throw IllegalArgumentException()
+    } else
+        return output.toIntArray()
 }
