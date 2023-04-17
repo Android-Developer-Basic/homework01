2246a9da7c9015941048b42ab331eb6c92036699
Dima_ <dima_87771995@mail.ru>
2023-04-17 15:38:40 +0300

    add realization in fizzbuzz function

--- src/main/kotlin/ru/otus/homework/fizzbuzz.kt
+++ src/main/kotlin/ru/otus/homework/fizzbuzz.kt
@@ -1,5 +1,21 @@
 package ru.otus.homework
 
 
-fun fizzbuzz(n: Int): Array<String> {
+fun fizzbuzz(n: Int): Array<String>  {
+    val outputArray: Array<String> = Array(n){""}
+
+    for (i in n - 1 downTo 0) {
+
+        if (i%3 == 0 && i%5 != 0) {
+            outputArray[i] = "Fizz"
+        } else if (i%5 == 0 && i%3 != 0) {
+            outputArray[i] = "Buzz"
+        } else if (i == 0 || i%3 == 0 && i%5 ==0) {
+            outputArray[i] = "FizzBuzz"
+        } else {
+            outputArray[i] = i.toString()
+        }
+    }
+
+    return outputArray
 }
