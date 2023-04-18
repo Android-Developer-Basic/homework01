827059dd6ebf501e0fc108e090226bcb174f1262
Aleksei Borodulia <a.borodulya@yahoo.com>
2023-04-18 17:05:57 +0300

    Добавил тело функции FizzBuzz в файле fizzbuzz.kt
    Добавил тело функции SumOfTwo в файле sumoftwo.kt

--- src/main/kotlin/ru/otus/homework/fizzbuzz.kt
+++ src/main/kotlin/ru/otus/homework/fizzbuzz.kt
@@ -2,4 +2,19 @@ package ru.otus.homework
 
 
 fun fizzbuzz(n: Int): Array<String> {
+
+    var intArr = arrayOf<Int>()
+    var stringArr = arrayOf<String>()
+
+    for (i in 0 until n) intArr += i
+    for (i in intArr) {
+        stringArr += if(i % 3 == 0 && i % 5 == 0) {
+            "FizzBuzz"
+        } else if(i % 3 == 0) {
+            "Fizz"
+        } else if(i % 5 == 0) {
+            "Buzz"
+        } else "$i"
+    }
+    return stringArr
 }
