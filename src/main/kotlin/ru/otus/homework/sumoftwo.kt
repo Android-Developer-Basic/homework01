b0c30ea664683f326952f0e2bd41a07321bdc7f8
Dima_ <dima_87771995@mail.ru>
2023-04-19 13:23:10 +0300

    add realization in sumOfTwo function

--- src/main/kotlin/ru/otus/homework/sumoftwo.kt
+++ src/main/kotlin/ru/otus/homework/sumoftwo.kt
@@ -2,4 +2,27 @@ package ru.otus.homework
 
 
 fun sumOfTwo(numbers: IntArray, target: Int): IntArray {
+    val arrIndexes = IntArray(2)
+    var sum: Int
+
+    if (numbers.size < 2) throw IllegalArgumentException()
+
+    for ((k, value) in numbers.withIndex()) {
+        for ((i, value) in numbers.withIndex()) {
+            if (k == i) {
+                continue
+            } else {
+                sum = numbers[k] + numbers[i]
+
+                if (sum == target) {
+                    arrIndexes[0] = i
+                    arrIndexes[1] = k
+                }
+            }
+        }
+    }
+
+    if (arrIndexes[0] == 0 && arrIndexes[1] == 0) throw IllegalArgumentException()
+
+    return arrIndexes
 }
