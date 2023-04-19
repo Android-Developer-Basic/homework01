c556ef9526097e753d2cb43724e1f4ee3946b5b6
Andrey Efimov <aefimovs@yandex.ru>
2023-04-19 22:47:25 +0300

    homework 1

--- src/main/kotlin/ru/otus/homework/fizzbuzz.kt
+++ src/main/kotlin/ru/otus/homework/fizzbuzz.kt
@@ -2,4 +2,23 @@ package ru.otus.homework
 
 
 fun fizzbuzz(n: Int): Array<String> {
+    val intArray = IntArray(n) { it }
+    var stringArray = arrayOf<String>()
+
+    for (i in intArray) {
+        val isMultipleThree = i % 3 == 0
+        val isMultipleFive = i % 5 == 0
+
+        stringArray += if (i == 0 || (isMultipleThree && isMultipleFive)) {
+            "FizzBuzz"
+        } else if (isMultipleThree) {
+            "Fizz"
+        } else if (isMultipleFive) {
+            "Buzz"
+        } else {
+            i.toString()
+        }
+    }
+
+    return stringArray
 }
