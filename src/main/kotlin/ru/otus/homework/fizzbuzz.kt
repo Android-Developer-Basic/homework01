c5464137260b675627ba130285f4afe2c58ff8fb
Aleksei Borodulia <a.borodulya@yahoo.com>
2023-04-19 17:48:31 +0300

    Переписал тело функции FizzBuzz в файле fizzbuzz.kt, избавился от массивов нулевой длины.

--- src/main/kotlin/ru/otus/homework/fizzbuzz.kt
+++ src/main/kotlin/ru/otus/homework/fizzbuzz.kt
@@ -1,20 +1,9 @@
 package ru.otus.homework
 
 
-fun fizzbuzz(n: Int): Array<String> {
-
-    var intArr = arrayOf<Int>()
-    var stringArr = arrayOf<String>()
-
-    for (i in 0 until n) intArr += i
-    for (i in intArr) {
-        stringArr += if(i % 3 == 0 && i % 5 == 0) {
-            "FizzBuzz"
-        } else if(i % 3 == 0) {
-            "Fizz"
-        } else if(i % 5 == 0) {
-            "Buzz"
-        } else "$i"
-    }
-    return stringArr
+fun fizzbuzz(n: Int): Array<String> = Array(n) { i ->
+    if (i % 3 == 0 && i % 5 == 0) "FizzBuzz"
+    else if(i % 3 == 0) "Fizz"
+    else if(i % 5 == 0) "Buzz"
+    else i.toString()
 }
