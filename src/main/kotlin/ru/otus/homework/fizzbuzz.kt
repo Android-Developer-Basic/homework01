9fb37773d979a133216e9942b55faa78ff6b8461
AndreiRookie <andrey.idem@gmail.com>
2023-06-09 14:00:17 +0400

    task 1 & 2 done

--- src/main/kotlin/ru/otus/homework/fizzbuzz.kt
+++ src/main/kotlin/ru/otus/homework/fizzbuzz.kt
@@ -2,4 +2,25 @@ package ru.otus.homework
 
 
 fun fizzbuzz(n: Int): Array<String> {
+
+    val intArray = IntArray(n)
+    (0 until n).forEach { i -> intArray[i] = i }
+
+    val stringArray = Array(size = n) {""}
+
+    intArray.forEach { i ->
+        when {
+            i == 0 || (i % 3 == 0 && i % 5 == 0)  -> {
+                stringArray[i] = "FizzBuzz"
+            }
+            i % 3 == 0 -> {
+                stringArray[i] = "Fizz"
+            }
+            i % 5 == 0 -> {
+                stringArray[i] = "Buzz"
+            }
+            else -> stringArray[i] = i.toString()
+        }
+    }
+   return stringArray
 }
