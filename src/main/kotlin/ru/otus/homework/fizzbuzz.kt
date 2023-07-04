e836a1baee7bf3aa3968b17087d72e50c3bb1223
Pak <helicopterpak@gmail.com>
2023-07-04 15:42:01 +0300

    done

--- src/main/kotlin/ru/otus/homework/fizzbuzz.kt
+++ src/main/kotlin/ru/otus/homework/fizzbuzz.kt
@@ -1,5 +1,23 @@
 package ru.otus.homework
 
 
-fun fizzbuzz(n: Int): Array<String> {
+fun fizzbuzz(n: Int): Array<String?> {
+    val stringArray = arrayOfNulls<String>(n)
+    val intArray = IntArray(n)
+
+    (0 until n).forEach { i ->
+        intArray[i] = i
+    }
+    if (n > 0) {
+        for (i in intArray) {
+            if ((intArray[i] % 5) == 0 && (intArray[i] % 3) == 0) {
+                stringArray[i] = "FizzBuzz"
+            } else if ((intArray[i] % 3) == 0 && intArray[i] != 0) {
+                stringArray[i] = "Fizz"
+            } else if ((intArray[i] % 5) == 0 && intArray[i] != 0) {
+                stringArray[i] = "Buzz"
+            } else stringArray[i] = intArray[i].toString()
+        }
+    }
+    return stringArray
 }
--- src/main/kotlin/ru/otus/homework/sumoftwo.kt
+++ src/main/kotlin/ru/otus/homework/sumoftwo.kt
@@ -1,5 +1,19 @@
 package ru.otus.homework
 
+import kotlin.IllegalArgumentException as IllegalArgumentException
+
 
 fun sumOfTwo(numbers: IntArray, target: Int): IntArray {
+    val result = IntArray(2)
+
+    for (i in 0 until numbers.size - 1) {
+        for (j in 1 until numbers.size) {
+            if ((numbers[i] + numbers[j]) == target) {
+                result[0] = i
+                result[1] = j
+                return result
+            }
+        }
+    }
+    throw IllegalArgumentException("no pairs")
 }
