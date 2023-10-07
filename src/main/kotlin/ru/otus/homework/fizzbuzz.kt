4f5ece5bb493a481fc92521e10e883fb9b2eee89
t-o-n-y-p <42129780+t-o-n-y-p@users.noreply.github.com>
2023-10-07 14:20:07 +0300

    Added homework1 solutions

--- src/main/kotlin/ru/otus/homework/fizzbuzz.kt
+++ src/main/kotlin/ru/otus/homework/fizzbuzz.kt
@@ -2,4 +2,17 @@ package ru.otus.homework
 
 
 fun fizzbuzz(n: Int): Array<String> {
+    val intArray = IntArray(n) { it }
+    var result = emptyArray<String>()
+    for (i in intArray) {
+        var e = ""
+        if (i % 3 == 0) {
+            e += "Fizz"
+        }
+        if (i % 5 == 0) {
+            e += "Buzz"
+        }
+        result += e.ifEmpty { i.toString() }
+    }
+    return result
 }
