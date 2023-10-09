0a4ce30ef66b6b8e8e650fe4c027d1665203c8a8
Ksiana <mari.serovatova@list.ru>
2023-10-09 18:50:28 +0900

    hw1 - fun fizzbuzz & sumOfTwo

--- src/main/kotlin/ru/otus/homework/fizzbuzz.kt
+++ src/main/kotlin/ru/otus/homework/fizzbuzz.kt
@@ -1,5 +1,25 @@
 package ru.otus.homework
 
+const val FIZZ_BUZZ = "FizzBuzz"
+const val FIZZ = "Fizz"
+const val BUZZ = "Buzz"
+const val SEPARATOR = ", "
+fun fizzbuzz(n: Int): List<String> {
+    val result = mutableListOf<String>()
 
-fun fizzbuzz(n: Int): Array<String> {
+    for (i in 0 until n) {
+        when {
+            (i % 3 == 0) && (i % 5 == 0) -> result.add(FIZZ_BUZZ)
+            (i % 3 == 0) -> result.add(FIZZ)
+            (i % 5 == 0) -> result.add(BUZZ)
+            else -> result.add(i.toString())
+        }
+    }
+    return result
+}
+
+fun main() {
+    val n = readln().toInt()
+    val fizzbuzzResult = fizzbuzz(n)
+    println(fizzbuzzResult.joinToString(SEPARATOR))
 }
