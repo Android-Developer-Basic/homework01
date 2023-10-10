fe78d5a78f47709a711702cff59d4495fcd4d11c
Валерий <RyabtsevVA1@it.mos.ru>
2023-10-10 18:35:01 +0300

    задача 1+задача 2

--- src/main/kotlin/ru/otus/homework/fizzbuzz.kt
+++ src/main/kotlin/ru/otus/homework/fizzbuzz.kt
@@ -2,4 +2,21 @@ package ru.otus.homework
 
 
 fun fizzbuzz(n: Int): Array<String> {
+    if (n == 0) {
+        return emptyArray()
+    }
+
+    val result = Array(n) { "" }
+
+    for (i in 0 until n) {
+
+        when {
+            i % 15 == 0 -> result[i] = "FizzBuzz"
+            i % 3 == 0 -> result[i] = "Fizz"
+            i % 5 == 0 -> result[i] = "Buzz"
+            else -> result[i] = i.toString()
+        }
+    }
+
+    return result
 }
