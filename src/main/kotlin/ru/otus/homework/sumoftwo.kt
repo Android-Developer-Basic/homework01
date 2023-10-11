5fc74e7cd2fb91bf58084609f27081ce0fa72c8d
glasolin <glasolin@gmail.com>
2023-10-11 18:59:37 +0300

    - fizzbuzz() function completed - sumOfTwo() function initial code

--- src/main/kotlin/ru/otus/homework/sumoftwo.kt
+++ src/main/kotlin/ru/otus/homework/sumoftwo.kt
@@ -2,4 +2,23 @@ package ru.otus.homework
 
 
 fun sumOfTwo(numbers: IntArray, target: Int): IntArray {
+/*    var found:Boolean=false;*/
+    numbers.forEachIndexed() { j, _ ->
+        var locSum:Int=0
+        var outIdx=ArrayList<Int>()
+        run breaking@ {
+            numbers.drop(j).forEachIndexed() { i, v ->
+                locSum += v
+                if (locSum <= target) {
+                    outIdx.add(i+j)
+                    if (locSum == target) {
+                        return outIdx.toIntArray()
+                    }
+                } else {
+                    return@breaking
+                }
+            }
+        }
+    }
+    throw IllegalArgumentException()
 }
