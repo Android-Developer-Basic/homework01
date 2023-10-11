6c6f144b4e1f22ed6a521c2a58e89611ca7d5f09
Ponyu-dev <mopoypa@yandex.ru>
2023-10-11 09:47:12 +0300

    Solution to problem number 2. Implementation of the function sumOfTwo. I also added a check to see if the input array of integers is filled with data. If the array is not full, then we throw an exception. I have also added a test for this case.

--- src/main/kotlin/ru/otus/homework/sumoftwo.kt
+++ src/main/kotlin/ru/otus/homework/sumoftwo.kt
@@ -2,4 +2,14 @@ package ru.otus.homework
 
 
 fun sumOfTwo(numbers: IntArray, target: Int): IntArray {
+    if (numbers.isEmpty())
+        throw IllegalArgumentException("Input array 'numbers' is empty.")
+
+    val diffMap = mutableMapOf<Int, Int>()
+    for (i in numbers.indices) {
+        val complement = target - numbers[i]
+        diffMap[complement]?.let { return intArrayOf(it, i) }
+        diffMap[numbers[i]] = i
+    }
+    throw IllegalArgumentException("No two numbers sum up to the target.")
 }
