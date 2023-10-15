19ac4ca308fa10e6450320f500fbeb362901bf3c
Tuged <104727740+Tuged@users.noreply.github.com>
2023-10-16 05:01:39 +0800

    Update fizzbuzz.kt

--- src/main/kotlin/ru/otus/homework/fizzbuzz.kt
+++ src/main/kotlin/ru/otus/homework/fizzbuzz.kt
@@ -1,5 +1,27 @@
 package ru.otus.homework
 
 
-fun fizzbuzz(n: Int): Array<String> {
+fun fizzbuzz(n: Int):Array<String?> {
+    var i = 0
+    var array = arrayOfNulls<String>(n)
+    while (i<n) {
+        if ((i%3==0) and (i%5==0)){
+            array[i] = "FizzBuzz"
+        }
+        else if (i%3==0) {
+            array[i] = "Fizz"
+        }
+        else if (i%5==0) {
+            array[i] = "Buzz"
+        }
+        else {
+            array[i] = i.toString()
+        }
+        i++
+    }
+    return array
+}
+
+fun main(){
+    print(fizzbuzz(readLine()!!.toInt()).contentToString())
 }
