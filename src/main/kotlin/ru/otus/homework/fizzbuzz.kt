9ad52a801443d9f044a2f17dca8be6e2fef3e1ec
Aleh Barushka <1.alehbarushka@gmail.com>
2023-10-15 13:52:26 +0300

    feat: add fizzbuzz and sumoftwo

--- src/main/kotlin/ru/otus/homework/fizzbuzz.kt
+++ src/main/kotlin/ru/otus/homework/fizzbuzz.kt
@@ -1,5 +1,21 @@
 package ru.otus.homework
 
-
 fun fizzbuzz(n: Int): Array<String> {
+    val fizzBuzz = "FizzBuzz"
+    val fizz = "Fizz"
+    val buzz = "Buzz"
+
+    val numbers = Array(n) { i -> i }
+    val strings = Array(n) { i -> i.toString() }
+
+    numbers.forEach { i ->
+        when {
+            (i % 3 == 0) && (i % 5 == 0) -> strings[i] = fizzBuzz
+            (i % 3 == 0) -> strings[i] = fizz
+            (i % 5 == 0) -> strings[i] = buzz
+            else -> strings[i] = "$i"
+        }
+    }
+
+    return strings
 }
--- src/main/kotlin/ru/otus/homework/sumoftwo.kt
+++ src/main/kotlin/ru/otus/homework/sumoftwo.kt
@@ -1,5 +1,16 @@
 package ru.otus.homework
 
-
 fun sumOfTwo(numbers: IntArray, target: Int): IntArray {
+    for (i in numbers.indices) {
+        numbers.forEachIndexed forEach@{ index, element ->
+            if (index == 0) return@forEach
+
+            if (numbers[i] + element == target) {
+                return intArrayOf(i, index)
+            }
+        }
+    }
+
+    throw IllegalArgumentException("not found")
 }
+
