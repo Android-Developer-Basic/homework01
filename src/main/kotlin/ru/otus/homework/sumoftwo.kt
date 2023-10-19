2ac777288da3a3e3cc1a9ddf41d3d8c671efe199
Gevorgiz <114309665+Gevorgiz@users.noreply.github.com>
2023-10-19 09:23:21 +0300

    homeWork1

--- src/main/kotlin/ru/otus/homework/sumoftwo.kt
+++ src/main/kotlin/ru/otus/homework/sumoftwo.kt
@@ -1,5 +1,12 @@
 package ru.otus.homework
 
-
 fun sumOfTwo(numbers: IntArray, target: Int): IntArray {
+    for ((firstIndex, firstNumber) in numbers.withIndex()) {
+        for (secondIndex in firstIndex + 1 until numbers.size) {
+            if (firstNumber + numbers[secondIndex] == target)
+                return intArrayOf(firstIndex, secondIndex)
+        }
+    }
+
+    throw IllegalArgumentException()
 }
