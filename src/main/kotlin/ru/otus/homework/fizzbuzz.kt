44f4b67fd501adefb8af954d6291a5148186d2e1
SR <serod1306@gmail.com>
2023-10-21 13:18:19 +0300

    homework

--- src/main/kotlin/ru/otus/homework/fizzbuzz.kt
+++ src/main/kotlin/ru/otus/homework/fizzbuzz.kt
@@ -2,5 +2,19 @@ package ru.otus.homework
 
 
 fun fizzbuzz(n: Int): Array<String> {
-    //todo
+    if(n == 0) { // т.к. n : Int, а не Int?, т.е. не nullable
+        return emptyArray()
+    }
+    var result = Array(n,{""})
+    for (i in 0..n-1) {
+        result[i] =
+        when (0) {
+            i, i % 3 + i % 5 -> "FizzBuzz"
+            i % 3 -> "Fizz"
+            i % 5 -> "Buzz"
+            else -> "$i"
+        }
+    }
+
+    return result
 }
--- src/main/kotlin/ru/otus/homework/sumoftwo.kt
+++ src/main/kotlin/ru/otus/homework/sumoftwo.kt
@@ -2,4 +2,15 @@ package ru.otus.homework
 
 
 fun sumOfTwo(numbers: IntArray, target: Int): IntArray {
+    if (numbers.size==0) throw IllegalArgumentException()
+
+    for (f in 0..numbers.size-1) {
+        for(k in f+1..numbers.size-1) {
+            if (numbers[f]+numbers[k] == target) {
+                return intArrayOf(f,k)
+            }
+        }
+    }
+
+    throw IllegalArgumentException()
 }
