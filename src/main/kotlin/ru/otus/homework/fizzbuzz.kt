26397475b902ced44f3b3e0e87a22bdb13f1aa69
Galina <118059223+Galinnka@users.noreply.github.com>
2023-11-04 14:30:58 +0300

    Homework01- 1 and 2

--- src/main/kotlin/ru/otus/homework/fizzbuzz.kt
+++ src/main/kotlin/ru/otus/homework/fizzbuzz.kt
@@ -1,23 +1,27 @@
 package ru.otus.homework
 
-
-fun fizzbuzz(n: Int) {
+fun fizbuzz(n:IntArray) {
     var i = 0
-    var array = arrayOfNulls<String>(n)
-    while (i<n) {
-        if ((i%3==0) and (i%5==0)){
-            array[i] = "FizzBuzz"
-        }
-        else if (i%3==0) {
-            array[i] = "Fizz"
-        }
-        else if (i%5==0) {
-            array[i] = "Buzz"
-        }
-        else {
-            array[i] = i.toString()
-        }
-        i++
+    val k1 = "Fizz"
+    val k2 = "Buzz"
+    println("Homework01-1")
+    while( i in n.indices){
+        val s = n[i]
+        val s1 = s%3
+        val s2 = s%5
+
+        if ((s1==0) && (s2==0) && (s==0)) { println("$s - $k1$k2") }
+        else if (s1==0) { println("$s - $k1") }
+        else if (s2==0) { println("$s - $k2") }
+        else println("$s - $s")
+
+        i++;
     }
-    print(array.contains(String()))
+}
+
+fun main() {
+    val nums = intArrayOf(0, 1, 2, 3, 4, 5,6,7,8,9,10)
+    fizbuzz(nums)
+    val nummmm = intArrayOf(2, 7, 11, 15)
+    sumOfTwo(nummmm, 9)
 }
