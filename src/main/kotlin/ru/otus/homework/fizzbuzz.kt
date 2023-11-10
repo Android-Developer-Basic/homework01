1c26b25cf47759f8783f60fd106992e9bdf8ec45
Anton <anton@Antons-MacBook-Air.local>
2023-11-10 17:59:29 +0300

    homework 1

--- src/main/kotlin/ru/otus/homework/fizzbuzz.kt
+++ src/main/kotlin/ru/otus/homework/fizzbuzz.kt
@@ -1,5 +1,33 @@
 package ru.otus.homework
 
+import java.util.*
+/*
+Функция `fizzbuzz` принимает целое число `n` размер массива чисел, который заполняется от 0. Реализуйте тело функции следующим образом:
 
+1. Создается массив чисел от 0 размерностью n
+2. Итерируемся по массиву чисел и заполняем выходной массив строк в зависимости от текущего числа:
+1. Если число кратно трем, то в массив добавляем строку `Fizz`
+2. Если число кратно пяти, то в массив добавляем строку `Buzz`
+3. Если число кратно и трем, и пяти, то в массив добавляем строку `FizzBuzz` (число 0 также должно добавлять строку `FizzBuzz`)
+4. Если число не кратно ни одному их этих чисел, то в массив нужно положить просто само число приведенное к строке
+*/
+
+fun checkValue(i: Int): String{
+    if(i%3 == 0){
+        if(i%5 == 0)return "FizzBuzz"
+        else return "Fizz"
+    }
+    if(i%5 == 0) return "Buzz"
+    return i.toString()
+}
 fun fizzbuzz(n: Int): Array<String> {
+    val random = Random()
+    val arr = Array<Int>(n){ random.nextInt(n) }
+    val result:Array<String> = Array<String>(n, {""})
+
+    for (index in 0 until arr.size){
+        result.set(index, checkValue(arr[index]))
+    }
+
+    return result
 }
--- src/main/kotlin/ru/otus/homework/sumoftwo.kt
+++ src/main/kotlin/ru/otus/homework/sumoftwo.kt
@@ -1,5 +1,22 @@
 package ru.otus.homework
 
+/* Функция `sumOfTwo` принимает массив целых чисел `numbers`, целое число `target`.
+
+Реализуйте тело функции следующим образом чтобы она возвращала массив целых чисел, состоящий из индексов элементов массива numbers так, чтобы сумма значений чисел находящихся на этих индексах была равна числу target.
+
+Если таких чисел нет, то выбросьте исключение `IllegalArgumentException` */
 
 fun sumOfTwo(numbers: IntArray, target: Int): IntArray {
+    val map = hashMapOf<Int, Int>()
+    for ((i,v) in numbers.withIndex()){
+        map[v] = i
+    }
+
+    for ((i,v) in numbers.withIndex()){
+        val delta = v - target;
+        val temp = map[delta]
+        if(temp != null && temp != i)return intArrayOf(temp, i)
+    }
+
+    throw IllegalArgumentException()
 }
