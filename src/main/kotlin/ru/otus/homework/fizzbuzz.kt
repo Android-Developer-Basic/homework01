acc24ca04ed6104c5e9f1372a50d853615b98e09
borodinus@rambler.ru <Borodinus@rambler.ru>
2023-12-23 22:37:16 +0300

    fizzbuzz and sumOfTwo

--- src/main/kotlin/ru/otus/homework/fizzbuzz.kt
+++ src/main/kotlin/ru/otus/homework/fizzbuzz.kt
@@ -2,4 +2,21 @@ package ru.otus.homework
 
 
 fun fizzbuzz(n: Int): Array<String> {
+    var i = 0
+    val arr = Array(n) { i++ }
+    val result = Array(n) { "" }
+    val fizz = "Fizz"
+    val buzz = "Buzz"
+    val fizzBuzz = "$fizz$buzz"
+
+    arr.forEach {
+        when{
+            it == 0 || it % 5 == 0 && it % 3 == 0 -> result[it] = fizzBuzz
+            it % 3 == 0 -> result[it] = fizz
+            it % 5 == 0 -> result[it] = buzz
+            else -> result[it] = it.toString()
+        }
+    }
+
+    return result
 }
