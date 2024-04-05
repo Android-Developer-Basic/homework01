cac58a45c5de97766fc15b0c25ee81e1225300d3
Kirill Ruban <krumsosh@gmail.com>
2024-04-05 14:13:05 +0300

    homework1

--- src/main/kotlin/ru/otus/homework/fizzbuzz.kt
+++ src/main/kotlin/ru/otus/homework/fizzbuzz.kt
@@ -2,5 +2,23 @@ package ru.otus.homework
 
 
 fun fizzbuzz(n: Int): Array<String> {
-    TODO("Выполните задание")
+    val generatedArray = IntArray(n){i -> i  }
+    val result = ArrayList<String>()
+    for (ii in 0..<n) {
+        var resultString = ""
+        if(generatedArray[ii] == 0){
+            resultString = "FizzBuzz"
+        }
+        if (generatedArray[ii] % 3 == 0) {
+            resultString="Fizz"
+        }
+        if (generatedArray[ii] % 5 == 0){
+            resultString += "Buzz"
+        }
+        if (resultString == "") {
+            resultString = generatedArray[ii].toString()
+        }
+        result.add(resultString)
+    }
+    return result.toTypedArray()
 }
--- src/main/kotlin/ru/otus/homework/sumoftwo.kt
+++ src/main/kotlin/ru/otus/homework/sumoftwo.kt
@@ -1,6 +1,40 @@
 package ru.otus.homework
 
 
-fun sumOfTwo(numbers: IntArray, target: Int): IntArray {
-    TODO("Выполните задание")
+fun sumOfTwo(numbers: IntArray, target: Int,first: Boolean =true): IntArray {
+
+    var result: ArrayList<Int> = ArrayList<Int>()
+    for (ii in 0..<numbers.size) {
+        result = ArrayList<Int>()
+        if (numbers[ii] > target) {
+            continue
+        }
+        result.add(ii)
+        if (numbers[ii] == target) {
+            break
+        }
+        val innerResult =
+            sumOfTwo(
+                numbers.copyOfRange(ii + 1, numbers.size),
+                target - numbers[ii],
+                false
+            ).asList()
+        if (innerResult.size > 0) {
+            for (elem in innerResult) {
+                result.add(elem + ii + 1)
+            }
+            break
+        }
+
+    }
+
+    var sum=0
+    for (index in result){
+        sum += numbers[index]
+    }
+    if (first && sum!=target) {
+        throw IllegalArgumentException()
+    }else{
+        return result.toIntArray()
+    }
 }
