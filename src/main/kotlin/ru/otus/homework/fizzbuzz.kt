200b5700e2d92836b6151a3245c91ecaeef5969a
Nikolay (nazarovNV) <nikolaynazarovvv216@gmail.com>
2024-04-07 00:19:45 +0300

    Сделал оба задания, поправил варнинги

--- src/main/kotlin/ru/otus/homework/fizzbuzz.kt
+++ src/main/kotlin/ru/otus/homework/fizzbuzz.kt
@@ -1,6 +1,38 @@
 package ru.otus.homework
 
-
+/**
+ * @param n - целое число, размер массива чисел
+ * @return - массив строк который итерируется по массиву чисел и 0 до n и заполняется
+ * следующим образом:
+ * 1. Если число кратно трем, то в массив добавляем строку`Fizz`
+ * 2. Если число кратно пяти, то в массив добавляем строку`Buzz`
+ * 3. Если число кратно и трем, и пяти, то в массив добавляем строку`FizzBuzz`
+ * (число 0 также должно добавлять строку `FizzBuzz`)
+ * 4. Если число не кратно ни одному их этих чисел, то в массив нужно положить просто само число
+ * приведенное к строке
+ */
 fun fizzbuzz(n: Int): Array<String> {
-    TODO("Выполните задание")
+    val fizzbuzzIntArray = Array(n){ it }
+    var fizzbuzzStringArray = emptyArray<String>()
+
+    for (item : Int in fizzbuzzIntArray){
+        fizzbuzzStringArray = if (item % 3 == 0 && item % 5 == 0)
+            fizzbuzzStringArray.plus("FizzBuzz")
+        else if (item % 3 == 0)
+            fizzbuzzStringArray.plus("Fizz")
+        else if (item % 5 == 0)
+            fizzbuzzStringArray.plus("Buzz")
+        else {
+            fizzbuzzStringArray.plus(item.toString())
+        }
+    }
+    //return fizzbuzzStringArray
+    return fizzbuzzStringArray
 }
+
+// Тут вызов для отладки и проверки
+//fun main(){
+//    for (i : String in fizzbuzz(10)){
+//        println(i)
+//    }
+//}
--- src/main/kotlin/ru/otus/homework/sumoftwo.kt
+++ src/main/kotlin/ru/otus/homework/sumoftwo.kt
@@ -1,6 +1,44 @@
 package ru.otus.homework
 
-
+/**
+ * @param numbers массив целых чисел numbers
+ * @param target целое число
+ * @return массив целых чисел, состоящий из индексов элементов массива numbers,
+ * сумма значений чисел находящихся на этих индексах равна числу target.
+ */
 fun sumOfTwo(numbers: IntArray, target: Int): IntArray {
-    TODO("Выполните задание")
+    var resultArray = IntArray(0)
+    for (i : Int in numbers.indices){
+        var a = 0
+        if (a == i)
+            a++
+        try {
+            while (numbers[i] + numbers[a] != target){
+                a++
+                if (a == i)
+                    a++
+            }
+            resultArray = resultArray.plus(i)
+            resultArray = resultArray.plus(a)
+            break
+        }
+        catch (e: ArrayIndexOutOfBoundsException) {
+            println("Перехват ошибки \"ArrayIndexOutOfBoundsException\"")
+        }
+    }
+    try {
+        resultArray [0]
+    }
+    catch (e: ArrayIndexOutOfBoundsException) {
+        throw IllegalArgumentException("Прокидываем ошибку \"IllegalArgumentException\"")
+    }
+    return resultArray
 }
+
+// Тут вызов для отладки и проверки
+//fun main(){
+//    val Array2 = intArrayOf(3, 2)
+//    for (i : Int in sumOfTwo(Array2, 6))
+//        println(i)
+//
+//}
