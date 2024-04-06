c3951033ce8dc79630519870974023e513dd1636
Gimasteev <best.roma7@gmail.com>
2024-04-06 13:40:28 +0500

    I did the first task

--- src/main/kotlin/ru/otus/homework/fizzbuzz.kt
+++ src/main/kotlin/ru/otus/homework/fizzbuzz.kt
@@ -1,6 +1,23 @@
 package ru.otus.homework
-
+//import Array
 
 fun fizzbuzz(n: Int): Array<String> {
-    TODO("Выполните задание")
+    //TODO("Выполните задание")
+    var myArray = emptyArray<String>()
+    if(n > 0) {
+        myArray = Array(n) { i -> i.toString() }
+        for (i in 0 until n) {
+            if (i == 0 || (i % 3 == 0 && i % 5 == 0)) {
+                myArray[i] = "FizzBuzz"
+            } else if (i % 3 == 0) {
+                myArray[i] = "Fizz"
+            } else if (i % 5 == 0) {
+                myArray[i] = "Buzz"
+            } else {
+                i.toString()
+            }
+        }
+
+    }
+    return myArray
 }
