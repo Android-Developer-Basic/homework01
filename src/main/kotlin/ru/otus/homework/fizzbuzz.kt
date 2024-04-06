44b8a1d59f8f4d9eea4974c40105cb49f71dc01c
dimangty <dimangty@mail.ru>
2024-04-06 17:54:11 +0700

    Homework

--- src/main/kotlin/ru/otus/homework/fizzbuzz.kt
+++ src/main/kotlin/ru/otus/homework/fizzbuzz.kt
@@ -2,5 +2,17 @@ package ru.otus.homework
 
 
 fun fizzbuzz(n: Int): Array<String> {
-    TODO("Выполните задание")
+    var array = Array<String>(n) { "" }
+    for(i in array.indices) {
+       if (i % 3 == 0 && i % 5 == 0) {
+           array[i] = "FizzBuzz"
+       } else if (i % 3 == 0) {
+           array[i] = "Fizz"
+       } else if (i % 5 == 0) {
+           array[i] = "Buzz"
+       } else {
+           array[i] = i.toString()
+       }
+    }
+    return array
 }
