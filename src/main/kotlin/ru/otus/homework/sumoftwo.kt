eebf60d4281d7bef607e96169abed67ae51d81b9
Gimasteev <best.roma7@gmail.com>
2024-04-07 12:01:43 +0500

    I did the second task

--- src/main/kotlin/ru/otus/homework/sumoftwo.kt
+++ src/main/kotlin/ru/otus/homework/sumoftwo.kt
@@ -2,5 +2,23 @@ package ru.otus.homework
 
 
 fun sumOfTwo(numbers: IntArray, target: Int): IntArray {
-    TODO("Выполните задание")
+    var list = mutableListOf<Int>()
+    var sum = 0
+    //var good = 0
+    for (i in 0 until numbers.size) {
+        sum = numbers[i]
+        for (j in i + 1 until numbers.size) // i = 1
+        {
+            if (sum + numbers[j] == target)
+            {
+                list.add(i)
+                list.add(j)
+            }
+        }
+    }
+    if(list.isEmpty())
+        throw IllegalArgumentException("Shit happens")
+
+    return list.toIntArray()
 }
+
