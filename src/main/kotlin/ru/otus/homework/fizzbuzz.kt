43e4ea5bf73a2c9dd43cb2717cf3a61a0d84ee72
Anton Kazakov <antonanton08@gmail.com>
2024-04-10 20:09:16 +0400

    Домашнее задание №1

--- src/main/kotlin/ru/otus/homework/fizzbuzz.kt
+++ src/main/kotlin/ru/otus/homework/fizzbuzz.kt
@@ -1,6 +1,36 @@
 package ru.otus.homework
 
+import kotlin.random.Random
 
-fun fizzbuzz(n: Int): Array<String> {
-    TODO("Выполните задание")
+
+fun main() {
+    val array = fizzbuzz(5)
+}
+
+
+
+fun fizzbuzz(x: Int): IntArray {
+    val array = IntArray(x)
+    var arr = emptyArray<String>()
+    var stroka: String = ""
+
+    for (i in 0 until array.size) {
+        array[i] = Random.nextInt(0, 100)
+        stroka = ""
+        if (array[i] % 3 == 0){
+            stroka += "Fizz"
+        }
+        if (array[i] % 5 == 0){
+            stroka += "Buzz"
+        }
+        if ((array[i] % 3 != 0) and (array[i] % 5 != 0)){
+            stroka += array[i].toString()
+        }
+        println(array[i])
+        arr += stroka
+        println(arr[i])
+    }
+
+    return array
+
 }
--- src/main/kotlin/ru/otus/homework/sumoftwo.kt
+++ src/main/kotlin/ru/otus/homework/sumoftwo.kt
@@ -1,6 +1,39 @@
 package ru.otus.homework
 
 
-fun sumOfTwo(numbers: IntArray, target: Int): IntArray {
-    TODO("Выполните задание")
+import kotlin.random.Random
+fun main(){
+    val numbers = IntArray(5)
+
+    val target = 7
+    for (i in 0 until numbers.size) {
+        numbers[i] = Random.nextInt(0, 10)
+        println("индекс " + i + " элемент " + numbers[i])
+    }
+    val a = sumOfTwo(numbers, target)
+    for (i in 0 until a.size ){
+        println("Новый массив " + a[i])
+    }
+}
+
+
+fun sumOfTwo(numbers: IntArray, target: Int): Array<Int> {
+    var ind = emptyArray<Int>()
+
+    for (i in 0 until numbers.size) {
+        for (j in i + 1 until numbers.size){
+
+            if (numbers[i] + numbers[j] == target)  {
+                ind += i
+                ind += j
+            }
+
+        }
+    }
+
+    if (ind.size == 0) {
+        throw IllegalArgumentException ("Нет элементов массива удовлетворяющих условию")
+    }
+
+    return ind
 }
