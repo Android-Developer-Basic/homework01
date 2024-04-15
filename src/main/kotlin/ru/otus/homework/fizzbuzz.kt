4201e24f43ebe2bfbbbd75ce7298842bbdc58b2b
DariaMerkusheva <merkushevada@eriskip.com>
2024-04-15 19:23:36 +0500

    Решены задачи 1 (fizzbuzz) и 2 (sumOfTwo)

--- src/main/kotlin/ru/otus/homework/fizzbuzz.kt
+++ src/main/kotlin/ru/otus/homework/fizzbuzz.kt
@@ -2,5 +2,27 @@ package ru.otus.homework
 
 
 fun fizzbuzz(n: Int): Array<String> {
-    TODO("Выполните задание")
+
+    val ints = Array(n) { i -> (i) }
+    val strings = Array(n) { "" }
+
+    for (i in 0 until n) {
+        if (i == 0) {
+            strings[i] = "FizzBuzz"
+        }
+        else if ((ints[i] % 3 == 0) and (ints[i] % 5 == 0)){
+            strings[i] = "FizzBuzz"
+        }
+        else if (ints[i] % 3 == 0) {
+            strings[i] = "Fizz"
+        }
+        else if (ints[i] % 5 == 0) {
+            strings[i] = "Buzz"
+        }
+        else {
+            strings[i] = "$i"
+        }
+    }
+
+    return strings
 }
--- src/main/kotlin/ru/otus/homework/sumoftwo.kt
+++ src/main/kotlin/ru/otus/homework/sumoftwo.kt
@@ -2,5 +2,20 @@ package ru.otus.homework
 
 
 fun sumOfTwo(numbers: IntArray, target: Int): IntArray {
-    TODO("Выполните задание")
+
+    var intArr = IntArray(0){0}
+
+    for (i in 0 until (numbers.size - 1)){
+        for(j in (i + 1) until numbers.size){
+            if (numbers[i] + numbers[j] == target){
+                intArr += i
+                intArr += j
+            }
+        }
+    }
+
+    if (intArr.isEmpty()) {
+        throw IllegalArgumentException("No result")
+    }
+    return intArr
 }
