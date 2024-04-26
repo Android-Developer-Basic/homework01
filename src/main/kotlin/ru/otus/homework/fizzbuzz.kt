162a27dcce392c277b7d6df7d8a62c71e08665ff
POL <ilidan@inbox.ru>
2024-04-26 20:24:20 +0300

    Готово

--- src/main/kotlin/ru/otus/homework/fizzbuzz.kt
+++ src/main/kotlin/ru/otus/homework/fizzbuzz.kt
@@ -1,6 +1,22 @@
+@file:Suppress("UNREACHABLE_CODE")
+
 package ru.otus.homework
 
 
 fun fizzbuzz(n: Int): Array<String> {
-    TODO("Выполните задание")
-}
+
+        return Array(n) { i ->
+            when {
+                (i + 1) % 3 == 0 && (i + 1) % 5 == 0 -> "FizzBuzz"
+                (i + 1) % 3 == 0 -> "Fizz"
+                (i + 1) % 5 == 0 -> "Buzz"
+                else -> (i + 1).toString()
+            }
+        }
+    }
+
+    fun main() {
+        val n = 15 // Размер массива
+        val result = fizzbuzz(n)
+        println(result.joinToString())
+    }
--- src/main/kotlin/ru/otus/homework/sumoftwo.kt
+++ src/main/kotlin/ru/otus/homework/sumoftwo.kt
@@ -1,6 +1,31 @@
+@file:Suppress("UNREACHABLE_CODE")
+
 package ru.otus.homework
 
 
 fun sumOfTwo(numbers: IntArray, target: Int): IntArray {
-    TODO("Выполните задание")
-}
+
+        val map = mutableMapOf<Int, Int>() // Создаем карту для хранения чисел и их индексов
+        for (i in numbers.indices) {
+            val complement = target - numbers[i] // Вычисляем дополнение для текущего числа
+            if (map.containsKey(complement)) {
+                // Если дополнение уже присутствует в карте, возвращаем его индекс и текущий индекс
+                return intArrayOf(map[complement]!!, i)
+            }
+            // В противном случае сохраняем текущее число и его индекс в карту
+            map[numbers[i]] = i
+        }
+        // Если не найдено соответствующих чисел, выбрасываем исключение IllegalArgumentException
+        throw IllegalArgumentException("No two sum solution")
+    }
+
+    fun main() {
+        val numbers = intArrayOf(2, 7, 11, 15)
+        val target = 9
+        try {
+            val result = sumOfTwo(numbers, target)
+            println("Индексы чисел, дающих сумму $target: ${result.joinToString()}")
+        } catch (e: IllegalArgumentException) {
+            println("Исключение: ${e.message}")
+        }
+    }
