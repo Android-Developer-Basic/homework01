352486dc4db8db121c14e5fa6906ce9f08374a90
DmitriiSmirnovD <9112252942s@gmail.com>
2024-06-18 11:20:13 +0300

    home work 1

--- src/main/kotlin/ru/otus/homework/fizzbuzz.kt
+++ src/main/kotlin/ru/otus/homework/fizzbuzz.kt
@@ -2,5 +2,17 @@ package ru.otus.homework
 
 
 fun fizzbuzz(n: Int): Array<String> {
-    TODO("Выполните задание")
+    val a = Array (n) {""}
+    for (i in 0.. n-1){
+        if (i % 3 == 0 && i % 5==0) {
+            a[i] = "FizzBuzz"
+        } else if (i % 3 == 0) {
+            a[i] = "Fizz"
+        } else if (i % 5 == 0) {
+            a[i] = "Buzz"
+        } else {
+            a[i] = "$i"
+        }
+    }
+    return a
 }
