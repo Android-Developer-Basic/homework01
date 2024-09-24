9fad72ae3f0ddaa9b798c270f052464f52685ec0
Andrusov <paandrusov@gid.ru>
2024-09-24 15:24:24 +0300

    Выполнено ДЗ homework01

--- src/main/kotlin/ru/otus/homework/fizzbuzz.kt
+++ src/main/kotlin/ru/otus/homework/fizzbuzz.kt
@@ -1,6 +1,17 @@
 package ru.otus.homework
 
-
 fun fizzbuzz(n: Int): Array<String> {
-    TODO("Выполните задание")
+    val array = Array(n) { it }
+    var result = emptyArray<String>()
+
+    for (x in array) {
+        result += when {
+            x == 0 || (x % 3 == 0 && x % 5 == 0) -> "FizzBuzz"
+            x % 3 == 0 -> "Fizz"
+            x % 5 == 0 -> "Buzz"
+            else -> x.toString()
+        }
+    }
+
+    return result
 }
