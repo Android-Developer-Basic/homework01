4f206f6bc49136aadce206420f39b84cf89f95be
OlegD <demonshine1983@gmail.com>
2024-10-07 21:09:15 +0300

    created both homework

--- src/main/kotlin/ru/otus/homework/fizzbuzz.kt
+++ src/main/kotlin/ru/otus/homework/fizzbuzz.kt
@@ -2,5 +2,27 @@ package ru.otus.homework
 
 
 fun fizzbuzz(n: Int): Array<String> {
-    TODO("Выполните задание")
+    var array: Array<String> = emptyArray()
+
+    for (i in 0..<n) {
+        array += when {
+            i % 3 == 0 && i % 5 == 0 -> {
+                "FizzBuzz"
+            }
+
+            i % 5 == 0 -> {
+                "Buzz"
+            }
+
+            i % 3 == 0 -> {
+                "Fizz"
+            }
+
+            else -> {
+                i.toString()
+            }
+        }
+    }
+
+    return array
 }
