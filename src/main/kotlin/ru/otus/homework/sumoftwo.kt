ac3de1805211989775a38994dd43b4820ea73623
Альберт <rubberbigpepper@gmail.com>
2024-10-08 20:15:04 +0500

    made speed to O(nlog(n))

--- src/main/kotlin/ru/otus/homework/sumoftwo.kt
+++ src/main/kotlin/ru/otus/homework/sumoftwo.kt
@@ -1,12 +1,34 @@
 package ru.otus.homework
 
+fun makeSortedNumbers(numbers: IntArray): Array<Pair<Int, Int>>{
+    val orderedNumbers = Array(numbers.size) {Pair(0,0)}
+    for((index, number) in numbers.withIndex()){
+        orderedNumbers[index] = Pair(number, index)
+    }
+    orderedNumbers.sortWith { left, right ->
+        when {
+            left.first < right.first -> -1
+            left.first > right.first -> 1
+            else -> 0
+        }
+    }
+    return orderedNumbers
+}
 
 fun sumOfTwo(numbers: IntArray, target: Int): IntArray {
-    for (i in 0..numbers.lastIndex) {
-        for (m in i + 1..numbers.lastIndex) {
-            if (numbers[i] + numbers[m] == target)
-                return intArrayOf(i, m)
-        }
+    val orderedNumbers = makeSortedNumbers(numbers)
+
+    var begin = 0
+    var end = orderedNumbers.lastIndex
+
+    while(begin != end) {
+        val sum = orderedNumbers[begin].first + orderedNumbers[end].first
+        if (sum == target)
+            return intArrayOf(orderedNumbers[begin].second, orderedNumbers[end].second)
+        if (sum < target)
+            ++begin
+        if (sum > target)
+            --end
     }
     throw IllegalArgumentException()
 }
