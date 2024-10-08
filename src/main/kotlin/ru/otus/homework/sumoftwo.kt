b679cffd95d0735bafdf71c2bbdd777d1408dd41
Evgenii Balandin <e.balandin@dt1520.ru>
2024-10-08 19:31:34 +0300

    Выполнил домашнее задание 1. Тесты проходит.

--- src/main/kotlin/ru/otus/homework/sumoftwo.kt
+++ src/main/kotlin/ru/otus/homework/sumoftwo.kt
@@ -2,5 +2,14 @@ package ru.otus.homework
 
 
 fun sumOfTwo(numbers: IntArray, target: Int): IntArray {
-    TODO("Выполните задание")
+    var i = -1
+    while (++i < numbers.size - 1)
+    {
+        for (j in i + 1 until numbers.size) {
+            if (numbers[i] + numbers[j] == target) {
+                return intArrayOf(i, j)
+            }
+        }
+    }
+    throw IllegalArgumentException()
 }
