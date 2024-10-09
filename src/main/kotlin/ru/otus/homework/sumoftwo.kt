3e6b4533a80a2894fc8588bef8c91a62b0d1a11d
Ilfirinon <Ilfirinon@yandex.com>
2024-10-10 04:33:43 +0800

    Add function sumoftwo

--- src/main/kotlin/ru/otus/homework/sumoftwo.kt
+++ src/main/kotlin/ru/otus/homework/sumoftwo.kt
@@ -2,5 +2,13 @@ package ru.otus.homework
 
 
 fun sumOfTwo(numbers: IntArray, target: Int): IntArray {
-    TODO("Выполните задание")
+    val numMap = mutableMapOf<Int, Int>()
+    for (i in numbers.indices) {
+        val complement = target - numbers[i]
+        if (numMap.containsKey(complement)) {
+            return intArrayOf(numMap[complement]!!, i)
+        }
+        numMap[numbers[i]] = i
+    }
+    throw IllegalArgumentException("No two sum solution")
 }
