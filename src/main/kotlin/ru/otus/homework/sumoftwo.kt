dd98e70f88af62dc78cb8c93e06e74e978abcb9c
SpiritualAdviser <pomaxam@narod.ru>
2024-10-11 11:24:46 +0300

    print returned array from fun sumOfTwo

--- src/main/kotlin/ru/otus/homework/sumoftwo.kt
+++ src/main/kotlin/ru/otus/homework/sumoftwo.kt
@@ -1,7 +1,7 @@
 package ru.otus.homework
 
 fun main() {
-    sumOfTwo(intArrayOf(2, 7, 11, 15, 3, 6), target = 9)
+    println(sumOfTwo(intArrayOf(2, 7, 11, 15), target = 9).contentToString())
 }
 
 fun sumOfTwo(numbers: IntArray, target: Int): IntArray {
