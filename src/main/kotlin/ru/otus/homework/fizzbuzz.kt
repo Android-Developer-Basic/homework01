a8736f0208971282c81f5fb617d011e0bbeaaf1c
SpiritualAdviser <pomaxam@narod.ru>
2024-10-11 11:11:13 +0300

    -- type of string array

--- src/main/kotlin/ru/otus/homework/fizzbuzz.kt
+++ src/main/kotlin/ru/otus/homework/fizzbuzz.kt
@@ -8,7 +8,7 @@ fun main() {
 fun fizzbuzz(n: Int): Array<String> {
     var i = 0
     val numbers = Array(n) { i++ }
-    val strings = Array<String>(numbers.size) {
+    val strings = Array(numbers.size) {
 
         when {
             it % 3 == 0 && it % 5 == 0->"FizzBuzz"
