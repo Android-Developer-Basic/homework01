507db7ff80fdbe61a0b2d0d5cbd8c17a02ea5147
SpiritualAdviser <pomaxam@narod.ru>
2024-10-11 11:15:19 +0300

    add fun main

--- src/main/kotlin/ru/otus/homework/sumoftwo.kt
+++ src/main/kotlin/ru/otus/homework/sumoftwo.kt
@@ -1,5 +1,8 @@
 package ru.otus.homework
 
+fun main() {
+    sumOfTwo(intArrayOf(2, 7, 11, 15, 3, 6), target = 9)
+}
 
 fun sumOfTwo(numbers: IntArray, target: Int): IntArray {
     TODO("Выполните задание")
