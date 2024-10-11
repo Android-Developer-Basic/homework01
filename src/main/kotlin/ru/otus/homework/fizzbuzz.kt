a36205b69a5cb7512b059715ba7831598f54b78d
SpiritualAdviser <pomaxam@narod.ru>
2024-10-11 11:07:14 +0300

    add when loop

--- src/main/kotlin/ru/otus/homework/fizzbuzz.kt
+++ src/main/kotlin/ru/otus/homework/fizzbuzz.kt
@@ -8,6 +8,16 @@ fun main() {
 fun fizzbuzz(n: Int): Array<String> {
     var i = 0
     val numbers = Array(n) { i++ }
-    val string = Array<String>(numbers.size) { "" }
+    val string = Array<String>(numbers.size) {
+
+        when {
+            it % 3 == 0 && it % 5 == 0->"FizzBuzz"
+            it % 3 == 0->"Fizz"
+            it % 5 == 0->"Buzz"
+            else-> {
+                it.toString()
+            }
+        }
+    }
     return string
 }
