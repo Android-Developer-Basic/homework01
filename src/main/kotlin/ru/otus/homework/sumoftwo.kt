7d73c96ec4321b279fa7fae154e730529c9fe17c
ivoylova_ev <ivoylova_ev@taximaxim.ru>
2024-10-13 20:43:51 +0500

    add homework#1 Control flow & Arrays

--- src/main/kotlin/ru/otus/homework/sumoftwo.kt
+++ src/main/kotlin/ru/otus/homework/sumoftwo.kt
@@ -1,6 +1,17 @@
 package ru.otus.homework
 
-
 fun sumOfTwo(numbers: IntArray, target: Int): IntArray {
-    TODO("Выполните задание")
+
+    for (i in numbers.indices){
+        for (j in i+1 ..<numbers.size){
+            val firstNum = numbers[i]
+            val secondNum = numbers[j]
+            if (firstNum + secondNum == target){
+                val resultArray = intArrayOf(i,j)
+                return resultArray
+            }
+        }
+    }
+
+    throw IllegalArgumentException()
 }
