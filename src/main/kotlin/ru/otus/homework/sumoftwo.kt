adf57fc1b95ac21a3f041a0bd698a09d77029d5d
Lofter <lofter23rus@gmail.com>
2024-10-15 21:47:36 +0300

    Kotlin #2 Задача #2 Домашнее задание

--- src/main/kotlin/ru/otus/homework/sumoftwo.kt
+++ src/main/kotlin/ru/otus/homework/sumoftwo.kt
@@ -1,6 +1,20 @@
 package ru.otus.homework
 
-
 fun sumOfTwo(numbers: IntArray, target: Int): IntArray {
-    TODO("Выполните задание")
+    // TODO("Выполните задание")
+    // Реализуйте тело функции следующим образом чтобы она возвращала массив целых чисел,
+    // состоящий из индексов элементов массива numbers так,
+    // чтобы сумма значений чисел находящихся на этих индексах была равна числу target.
+    val l_RetArray = IntArray(2)
+    for (i in 0 .. numbers.lastIndex) {
+        for (j in i+1..numbers.lastIndex) {
+            if (numbers[i] + numbers[j] == target) {
+                l_RetArray[0] = i;
+                l_RetArray[1] = j;
+                return l_RetArray
+            }
+        }
+    }
+    throw IllegalArgumentException()
 }
+
