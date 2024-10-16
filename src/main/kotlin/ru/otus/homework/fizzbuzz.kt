7e9f73677e03c2880c28fae680b890cb87d3d3ac
Валерий Асланов <valeryanatolievuch@gmail.com>
2024-10-16 09:06:05 +0500

    Homework 1, Control flow & Arrays, Valery Aslanov

--- src/main/kotlin/ru/otus/homework/fizzbuzz.kt
+++ src/main/kotlin/ru/otus/homework/fizzbuzz.kt
@@ -1,6 +1,20 @@
 package ru.otus.homework
 
-
 fun fizzbuzz(n: Int): Array<String> {
-    TODO("Выполните задание")
+    val result = mutableListOf<String>()
+    for (i in 0 until n) {
+        when {
+            i % 3 == 0 && i % 5 == 0 -> result.add("FizzBuzz")
+            i % 3 == 0 -> result.add("Fizz")
+            i % 5 == 0 -> result.add("Buzz")
+            else -> result.add(i.toString())
+        }
+    }
+    return result.toTypedArray()
+}
+
+fun main() {
+    val n = 5
+    val fizzBuzzOutput = fizzbuzz(n)
+    println(fizzBuzzOutput.joinToString(", "))
 }
