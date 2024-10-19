a611720334b199ab21ba6ae39cd0c68cc7d78ac3
ivanb <ivan.balakaev@gmail.com>
2024-10-19 21:16:35 +0300

    Added fun FizzBuzz and sumOfTwo

--- src/main/kotlin/ru/otus/homework/sumoftwo.kt
+++ src/main/kotlin/ru/otus/homework/sumoftwo.kt
@@ -1,6 +1,29 @@
 package ru.otus.homework
 
+fun main(args: Array<String>) {
+    val numbersArray = arrayOf(10, 7, 11, 15, 2)
+    val target = 9
+    println(sumOfTwo(numbersArray, target).contentToString())
 
-fun sumOfTwo(numbers: IntArray, target: Int): IntArray {
-    TODO("Выполните задание")
+}
+
+fun sumOfTwo(inpArray: Array<Int>, target: Int): Array<Int> {
+    val indexcesArray = arrayOf(0, 0)
+    inpArray.forEachIndexed { index, value ->
+
+        for (nextIndex in index + 1 until inpArray.size) {
+            val nextValue = inpArray[nextIndex]
+            val sumOfTwoValues = value + nextValue
+
+            if (sumOfTwoValues == target) {
+                indexcesArray[0] = index
+                indexcesArray[1] = nextIndex
+            }
+
+        }
+
+    }
+    if (indexcesArray[0] != 0 && indexcesArray[1] != 0) return indexcesArray else throw IllegalArgumentException("not values")
+
+
 }
