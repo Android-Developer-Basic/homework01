431ea12eafa720afddd5e657f820e4a9cb4562a8
ivanb <ivan.balakaev@gmail.com>
2024-10-19 21:26:01 +0300

    Change intArray to numbers Change arrayOf to intArrayOf

--- src/main/kotlin/ru/otus/homework/sumoftwo.kt
+++ src/main/kotlin/ru/otus/homework/sumoftwo.kt
@@ -1,18 +1,19 @@
 package ru.otus.homework
 
 fun main(args: Array<String>) {
-    val numbersArray = arrayOf(10, 7, 11, 15, 2)
+    val numbersArray = intArrayOf(10, 7, 11, 15, 2)
     val target = 9
     println(sumOfTwo(numbersArray, target).contentToString())
 
 }
 
-fun sumOfTwo(inpArray: Array<Int>, target: Int): Array<Int> {
-    val indexcesArray = arrayOf(0, 0)
-    inpArray.forEachIndexed { index, value ->
+fun sumOfTwo(numbers: IntArray, target: Int): IntArray {
 
-        for (nextIndex in index + 1 until inpArray.size) {
-            val nextValue = inpArray[nextIndex]
+    val indexcesArray = intArrayOf(0, 0)
+    numbers.forEachIndexed { index, value ->
+
+        for (nextIndex in index + 1 until numbers.size) {
+            val nextValue = numbers[nextIndex]
             val sumOfTwoValues = value + nextValue
 
             if (sumOfTwoValues == target) {
