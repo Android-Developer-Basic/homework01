cddd89d4dc8d94475f3f05ee31b3a315694bd937
Alekseev-i <Alekseev-i@inpas.ru>
2024-10-19 23:09:20 +0300

    Homework

--- src/main/kotlin/ru/otus/homework/fizzbuzz.kt
+++ src/main/kotlin/ru/otus/homework/fizzbuzz.kt
@@ -1,6 +1,15 @@
 package ru.otus.homework
 
-
 fun fizzbuzz(n: Int): Array<String> {
-    TODO("Выполните задание")
+    val arr = Array<String>(n) { "" }
+    for( i: Int in arr.indices ) {
+        arr[i] = when {
+            i == 0 -> "FizzBuzz"
+            i % 3 == 0 -> "Fizz"
+            i % 5 == 0 -> "Buzz"
+            (i % 5 == 0) && (i % 3 == 0) -> "FizzBuzz"
+            else -> i.toString()
+        }
+    }
+    return arr
 }
