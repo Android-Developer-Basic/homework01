1c3784cc561b325d951fb06a18926d1ccf6e4214
AlexeyVolkhin <volhin2012@gmail.com>
2024-11-18 15:20:17 +0500

    worked

--- src/main/kotlin/ru/otus/homework/fizzbuzz.kt
+++ src/main/kotlin/ru/otus/homework/fizzbuzz.kt
@@ -1,6 +1,24 @@
 package ru.otus.homework
 
+fun main() {
+    val value = 10
+    println(fizzbuzz(value).contentToString())
+}
 
 fun fizzbuzz(n: Int): Array<String> {
-    TODO("Выполните задание")
+    val listInt = (0..<n).toList()
+    var listString = arrayOf<String>()
+
+    for (i: Int in listInt) {
+        listString += if (i % 3 == 0 && i % 5 == 0) {
+            "FizzBuzz"
+        } else if (i % 3 == 0) {
+            "Fizz"
+        } else if (i % 5 == 0) {
+            "Buzz"
+        } else {
+            i.toString()
+        }
+    }
+    return listString
 }
